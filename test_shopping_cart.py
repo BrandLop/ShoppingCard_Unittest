e34b52a75f87519bcc3d6ff00784eddaaf7cd2a0
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-06-13 15:46:34 -0500

    Adding test_product_name method and test_product_price

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -18,6 +18,12 @@ class TestShoppingCart(unittest.TestCase):
         product = Product(name, price)  
         self.assertEqual(product.name, name)
         self.assertEqual(product.price, price, 'Price is not correct')
+    
+    def test_product_name(self):
+        self.assertEqual(self.smart_phone.name, self.name)
+    
+    def test_product_price(self):
+        self.assertEqual(self.smart_phone.price, self.price)
 
 if __name__ == '__main__':
     unittest.main()
