4601594e40d480640c702bd02c909c831d2de532
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-06-13 15:46:14 -0500

    Adding setUp method and tearDown

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -2,6 +2,15 @@ import unittest
 from product import Product
 
 class TestShoppingCart(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.name = 'Samsung'
+        self.price = 500.0
+        self.smart_phone = Product(self.name, self.price)
+
+    def tearDown(self) -> None:
+        pass
+
     def test_product_object(self):
         name = 'Apple'
         price = 1.70
