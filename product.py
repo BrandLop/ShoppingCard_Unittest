849b7689c0cfd29196ee33807481f018a61018ea
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-06-13 19:10:26 -0500

    Creating the ProductDiscountError class

--- product.py
+++ product.py
@@ -1,5 +1,11 @@
+class ProductDiscountError(Exception):
+    pass
+
 class Product:
     def __init__(self, name:str, price:float, discount:float=0.0) -> None:
         self.name = name
         self.price = price
+
+        if discount > price:
+            raise ProductDiscountError('Discount cannot be greater than price')
         self.discount = discount
